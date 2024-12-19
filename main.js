3e94efd745513cde542b0fc936299a329f209088
Umedov Umidjon <lustrum062@gmail.com>
2024-12-19 14:33:21 +0300

    previous

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
-import { sleep } from "./sleep.js";
-import { getRandomNumber } from "./random.js";
-import { Person } from "./person.js";
+// import { sleep } from "./sleep.js";
+// import { getRandomNumber } from "./random.js";
+// import { Person } from "./person.js";
 
-const person = new Person("Latifova Aziza", 2007);
+// const person = new Person("Latifova Aziza", 2007);
 
-const delay = getRandomNumber();
-console.log(`Waiting for: ${delay} milliseconds`);
+// const delay = getRandomNumber();
+// console.log(`Waiting for: ${delay} milliseconds`);
 
-sleep(delay).then(() => {
-  console.log(person.getInfo());
-});
+// sleep(delay).then(() => {
+//   console.log(person.getInfo());
+// });
+
+
+function getMillisecondsBetween(date1, date2) {
+  const num=new Date(date1)
+  const num2=new Date(date2)
+  return num.getSeconds
+}
